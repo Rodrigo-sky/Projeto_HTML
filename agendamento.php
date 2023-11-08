31ce1f7b43ccc564fa454b7b0f9c72dd4276026f
Rodrigo-sky <rtrs.eu@gmail.com>
2023-11-08 19:35:22 -0300

    terminado a pagina exames com a tabela de preços

--- agendamento.php
+++ agendamento.php
@@ -10,7 +10,7 @@
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 	<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
-	<title>Pet piscar</title>
+	<title>Pet Life</title>
 	<script>
         $(function() {
 			$('#inserir').click(function() {
